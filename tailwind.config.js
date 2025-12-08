5972375c5f339888a0fc4b3bc33a0198d6c24d7a
Chanansiri Kumyon <ernxq00@gmail.com>
2025-12-08 23:03:45 +0700

    landingpage 1st draft

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        cream: "#F3F0E6",
+        sand: "#C2B6A5",
+        plumDark: "#4E3338",
+
+        stone: "#D6CDC0",
+        clay: "#8C5547",
+        plumSoft: "#8A5F6D",
+
+        deepTeal: "#173436",
+        mutedTeal: "#3C5B5B",
+        smokey: "#4B4A45", }
+
+      },
+        fontfamily: {
+          body:['IM Fell Great Primer'],
+           thai: ['"Noto Sans Thai"', 'sans-serif'],
+         }
+    }
+}
+
+
